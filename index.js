6a6ebeb6887fc2cf6d712ae430b58818508d93d8
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-03 10:27:51 -0500

    Refactorizar mensaje de inicio del servidor para eliminar la referencia a "localhost"

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use('/api/events', eventsRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on ${PORT}`)
 })
