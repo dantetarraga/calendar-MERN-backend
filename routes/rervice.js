f902fc4342479f830c060f9865f37e58995a8b1e
Dante Tárraga <dante.tarragau@gmail.com>
2024-10-02 18:14:51 -0500

    feat: Agregar rutas para el servicio con métodos POST y GET

---
+++ routes/rervice.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+
+const serviceRouter = Router()
+
+serviceRouter.post('/', (req, res) => {
+  const data = req.body
+
+  res.json({
+    resp: data,
+    message: 'Service POST'
+  })
+})
+
+serviceRouter.get('/', (req, res) => {
+  res.json({
+    message: 'Service GET'
+  })
+})
